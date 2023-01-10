6f994527560d63ed3c29d3b9e537092eee876049
Leonardo Andrade <leonardo.andrade@epn.edu.ec>
2023-01-10 12:48:09 -0500

    Intent

--- 01-Android/app/src/main/java/com/example/movcomplmac/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movcomplmac/MainActivity.kt
@@ -1,13 +1,63 @@
 package com.example.movcomplmac
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import android.widget.Button
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
+
+    val contenidoIntentExplicito =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if (result.data != null){
+                    val data = result.data
+                    Log.i("intent-epn","${data?.getStringExtra("nombreModificado")}")
+                }
+            }
+
+        }
+
+
+    val contenidoIntentImplicito =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if (result.resultCode == Activity.RESULT_OK){
+                if (result.data != null){
+                    if (result.data!!.data != null){
+                        val uri: Uri = result.data!!.data!!
+                        val cursor = contentResolver.query(
+                            uri,
+                            null,
+                            null,
+                            null,
+                            null,
+                            null
+                        )
+                        cursor?.moveToFirst()
+                        val indiceTelefono = cursor?.getColumnIndex(
+                            ContactsContract.CommonDataKinds.Phone.NUMBER
+                        )
+                        val telefono = cursor?.getString(
+                            indiceTelefono!!
+                        )
+                        cursor?.close()
+                        Log.i("intent-epn","Telefono ${telefono}")
+                    }
+                }
+            }
+
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,7 +71,34 @@ class MainActivity : AppCompatActivity() {
         botonListView.setOnClickListener{
             irActividad(BListView::class.java)
         }
+
+        val botonIntentImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonIntentImplicito.setOnClickListener{
+            val intentConRespuesta = Intent(
+                Intent.ACTION_PICK,
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+            )
+            contenidoIntentImplicito.launch(intentConRespuesta)
+        }
+
+        val botonIntent = findViewById<Button>(R.id.btn_intent)
+        botonIntent.setOnClickListener{
+            abrirActividadConParametros(CIntentExplicitoParametros::class.java)
+        }
+
     }
+
+    fun abrirActividadConParametros(
+        clase: Class<*>
+    ){
+        val intentExplicito = Intent(this, clase)
+        intentExplicito.putExtra("nombre","Adrian")
+        intentExplicito.putExtra("apellido","Eguez")
+        intentExplicito.putExtra("edad",32)
+        contenidoIntentImplicito.launch(intentExplicito)
+    }
+
+
 
     fun irActividad(
         clase: Class<*>
