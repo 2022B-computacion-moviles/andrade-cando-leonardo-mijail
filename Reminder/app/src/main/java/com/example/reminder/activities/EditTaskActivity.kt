dc25879e015b09aa74612b00f6bbc8d6786de95c
RickC1218 <ricardo.erazo@epn.edu.ec>
2023-03-03 22:07:10 -0500

    create individual and edit classes of tasks and Subtasks

---
+++ Reminder/app/src/main/java/com/example/reminder/activities/EditTaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.reminder.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.reminder.R
+
+class EditTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_task)
+    }
+}
---
+++ Reminder/app/src/main/java/com/example/reminder/activities/IndividualSubtaskActivity.kt
@@ -0,0 +1,12 @@
+package com.example.reminder.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.reminder.R
+
+class IndividualSubtaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_individual_subtask)
+    }
+}
